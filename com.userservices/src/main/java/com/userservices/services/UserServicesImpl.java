d28b5416d09dde951b6c390bb03e1631d86d4bd6
hasnainhkhan <hhk2170@gmail.com>
2025-04-10 20:08:15 +0530

    service communication added

--- com.userservices/src/main/java/com/userservices/services/UserServicesImpl.java
+++ com.userservices/src/main/java/com/userservices/services/UserServicesImpl.java
@@ -1,21 +1,33 @@
 package com.userservices.services;
 
+import java.lang.System.Logger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import org.springframework.web.client.RestTemplate;
 import com.userservices.entities.UserEntity;
 import com.userservices.exception.ResourceNotFoundException;
 import com.userservices.repository.UserServiceRepo;
+
+
 
 @Component 
 public class UserServicesImpl implements UserServices{
 	
 	@Autowired
 	private UserServiceRepo userServiceRepo;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	// Logger
+	private org.slf4j.Logger logger = LoggerFactory.getLogger(UserServicesImpl.class);
 
 	@Override 
 	public UserEntity createUser(UserEntity users) {
@@ -32,8 +44,14 @@ public class UserServicesImpl implements UserServices{
 	
 	@Override 
 	public UserEntity findById(String id) {
-	    return userServiceRepo.findById(id)
+		ArrayList forObject =  restTemplate
+		.getForObject("http://localhost:8083/ratings/hotels/"+id
+				,ArrayList.class);
+		logger.info("{}",forObject);
+	    UserEntity user =  userServiceRepo.findById(id)
 	            .orElseThrow(() -> new ResourceNotFoundException("Resource not found on server: " + id));
+	    user.setRatings(forObject);
+	    return user;
 	}
 	
 	@Override

6804398c469c70f0de6c54823204e65cea272f71
hasnainhkhan <hhk2170@gmail.com>
2025-05-01 23:20:07 +0530

    fign flient added

--- com.userservices/src/main/java/com/userservices/Application.java
+++ com.userservices/src/main/java/com/userservices/Application.java
@@ -2,8 +2,10 @@ package com.userservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients //to inbuild feign client
 public class Application {
 
 	public static void main(String[] args) {
